9d4af28fc11bb3e9f178f0e1339cb8346e343279
iamstudybox00 <iamstudybox00@gmail.com>
2025-06-30 10:12:58 +0900

    Revert "브랜치"
    This reverts commit 12eda815b4c4673dab7c2a621c49e16d05a2b220.

--- LMSProject/src/main/java/com/lms/springboot/MainController.java
+++ LMSProject/src/main/java/com/lms/springboot/MainController.java
@@ -9,36 +9,7 @@ public class MainController
 	@GetMapping("/")
 	public String home()
 	{
-		return "mainPage";
+		return "home";
 	}	
 	
-	@GetMapping("/loginPage.do")
-	public String loginPage() {
-		return "auth/loginPage";
-	}
-	
-	
-	// 여기부터는 화면 확인용
-	@GetMapping("/myLectureList.do")
-	public String myLectureList() {
-		return "guest/myLectureList";
-	}
-	
-	@GetMapping("/lecture.do")
-	public String lecture() {
-		return "guest/lecture";
-	}
-	
-	@GetMapping("/lectureList.do")
-	public String lectureList() {
-		return "guest/lectureList";
-	}
-	@GetMapping("/lectureView.do")
-	public String lectureView() {
-		return "guest/lectureView";
-	}
-	@GetMapping("/lectureResource.do")
-	public String lectureResource() {
-		return "guest/lectureResource";
-	}
 }
