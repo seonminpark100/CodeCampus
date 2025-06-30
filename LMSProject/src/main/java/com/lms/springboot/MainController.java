12eda815b4c4673dab7c2a621c49e16d05a2b220
iamstudybox00 <iamstudybox00@gmail.com>
2025-06-30 10:07:46 +0900

    브랜치

--- LMSProject/src/main/java/com/lms/springboot/MainController.java
+++ LMSProject/src/main/java/com/lms/springboot/MainController.java
@@ -9,7 +9,36 @@ public class MainController
 	@GetMapping("/")
 	public String home()
 	{
-		return "home";
+		return "mainPage";
 	}	
 	
+	@GetMapping("/loginPage.do")
+	public String loginPage() {
+		return "auth/loginPage";
+	}
+	
+	
+	// 여기부터는 화면 확인용
+	@GetMapping("/myLectureList.do")
+	public String myLectureList() {
+		return "guest/myLectureList";
+	}
+	
+	@GetMapping("/lecture.do")
+	public String lecture() {
+		return "guest/lecture";
+	}
+	
+	@GetMapping("/lectureList.do")
+	public String lectureList() {
+		return "guest/lectureList";
+	}
+	@GetMapping("/lectureView.do")
+	public String lectureView() {
+		return "guest/lectureView";
+	}
+	@GetMapping("/lectureResource.do")
+	public String lectureResource() {
+		return "guest/lectureResource";
+	}
 }
