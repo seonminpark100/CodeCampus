011c38d3b22088e73453e460680a7c7ddf691e94
yahoozhf <yahoozhf@naver.com>
2025-07-15 16:26:44 +0900

    1111

--- LMSProject/src/main/java/com/lms/springboot/LmsProjectApplication.java
+++ LMSProject/src/main/java/com/lms/springboot/LmsProjectApplication.java
@@ -1,10 +1,8 @@
 package com.lms.springboot;
 
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.context.ApplicationContext;
 
 
