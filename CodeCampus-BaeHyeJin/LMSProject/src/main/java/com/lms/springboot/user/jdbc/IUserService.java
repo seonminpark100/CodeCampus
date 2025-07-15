6aa39909331a4a3cea484ecb63c143d4a3f91e34
yahoozhf <yahoozhf@naver.com>
2025-07-15 16:17:32 +0900

    step7-2 파일삭제

--- CodeCampus-BaeHyeJin/LMSProject/src/main/java/com/lms/springboot/user/jdbc/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.lms.springboot.user.jdbc;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface IUserService
-{	
-	public UserFileDTO selectOneFile(String sfile);
-}
--- CodeCampus-BaeHyeJin/LMSProject/src/main/resources/static/js/commons.js
+++
@@ -1,7 +0,0 @@
-function myAlert(){
-	alert("경고창이 표시됩니다.")
-}
-
-let myConsole = function(param){
-	console.log("파라미터: " + param);
-}
