8ca13d968eb05f2b3e87df1acc177e9d89c86d38
yahoozhf <yahoozhf@naver.com>
2025-07-15 16:13:54 +0900

    7-1 mapperscan 제거

--- LMSProject/src/main/java/com/lms/springboot/LmsProjectApplication.java
+++ LMSProject/src/main/java/com/lms/springboot/LmsProjectApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.lms.springboot.jdbc")
+//@MapperScan(basePackages = "com.lms.springboot.jdbc")
 public class LmsProjectApplication {
 
     public static void main(String[] args) {
